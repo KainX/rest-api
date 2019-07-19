b823f08257c7fb3772fd4f6a8cc0ee944c0f36cd
Kyu <rx1914@hotmail.com>
2019-07-18 23:38:03 -0500

    gitignore fix

--- target/classes/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.example.javaee
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class JAXRSConfiguration extends Application {
-
-
-}
